74010428bd0799e3de1e98cbb60060addc360e08
Gustavo Chimoy Garcia <gustavo_fred@hotmail.com>
2022-06-23 19:19:56 -0500

    actualizaciones

---
+++ ReportesPDF02/Models/DaoPeru.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReportesPDF02.Models
+{
+    public class DaoPeru
+    {
+        public List<departamentos> peruDepaProv()
+        {
+            List<departamentos> list = new List<departamentos>();
+
+            using(var db=new ModelPeru())
+            {
+                var query = from depa in db.departamentos select depa;
+
+                foreach(var depa in query)
+                {
+                    departamentos departamento = depa;
+
+                    foreach(var prov in depa.provincias)
+                    {
+                        departamento.provincias.Add(prov);
+                    }
+
+                    list.Add(departamento);
+                }
+            }
+
+            return list;
+        }
+    }
+}
